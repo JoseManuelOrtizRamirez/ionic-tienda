26f3e9e2376c6ef3351f9204f660b207c5a9a7d6
David117M-a <77523267+David117M-a@users.noreply.github.com>
2023-05-24 18:34:22 -0600

    Fixes

--- src/components/cardItem.vue
+++ src/components/cardItem.vue
@@ -1,12 +1,12 @@
 <template>
-    <ion-card>
+    <ion-card class="card">
         <img
             src="https://images.unsplash.com/photo-1613585810030-c2ae396ce0ff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" />
-        <ion-card-header>
-            <ion-card-title>Cheetos</ion-card-title>
-            <ion-card-subtitle>$16.00</ion-card-subtitle>
+        <ion-card-header class="card-header">
+            <ion-card-title class="card-title">Cheetos</ion-card-title>
+            <ion-card-subtitle class="card-subtitle">$16.00</ion-card-subtitle>
         </ion-card-header>
-        <ion-card-content>
+        <ion-card-content class="card-content">
             <p class="description">Here's a small text description for the card content. Nothing more, nothing less.</p>
         </ion-card-content>
     </ion-card>
